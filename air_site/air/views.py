65a362a2dae416ef35e32fe4139646ccfec2fa67
Alena Kozlova <alena.kozlova2004@mail.ru>
2024-12-11 01:16:48 +0300

    add flights page

--- air_site/air/views.py
+++ air_site/air/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import HttpResponse, render
 
-from .models import Airplane
+from .models import Airplane, Flight
 
 
 def index(request):
@@ -8,7 +8,8 @@ def index(request):
 
 
 def flights(request):
-    return HttpResponse('flights')
+    flights = Flight.objects.all()
+    return render(request, 'flights.html', {'flights': flights})
 
 
 def booking(request):
