5642dd937fcc8e3d7baa37fbf106884dc4d44dca
Alena Kozlova <alena.kozlova2004@mail.ru>
2024-12-11 00:48:01 +0300

    add main page

--- air_site/air/urls.py
+++ air_site/air/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('flights/', views.flights, name='flights'),
+    path('booking/', views.booking, name='booking'),
+    path('search/', views.search, name='search'),
+    path('aircraft/', views.aircraft, name='aircraft'),
 ]
--- air_site/air/views.py
+++ air_site/air/views.py
@@ -1,5 +1,21 @@
-from django.shortcuts import HttpResponse
+from django.shortcuts import HttpResponse, render
 
 
 def index(request):
-    return HttpResponse(')')
+    return render(request, 'index.html')
+
+
+def flights(request):
+    return HttpResponse('flights')
+
+
+def booking(request):
+    return HttpResponse('booking')
+
+
+def search(request):
+    return HttpResponse('search')
+
+
+def aircraft(request):
+    return HttpResponse('aircraft')
